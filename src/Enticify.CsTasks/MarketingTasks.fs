90d2e6b432b667c741ec8c09fb20934f30780d19
Ben Taylor <ben.taylor.uk@gmail.com>
2012-08-31 11:16:01 +0100

    Added ability to delete advertisments and direct mail.  Factored out the code duplication into a couple of higher order functions.

--- src/Enticify.CsTasks/MarketingTasks.fs
+++ src/Enticify.CsTasks/MarketingTasks.fs
@@ -9,46 +9,55 @@ module CsTasks.Marketing
     let MarketingContextSingleton siteName =
         let mc = MarketingContext.Create(siteName, null, AuthorizationMode.NoAuthorization)
         fun () -> mc
-        
-    let DeleteExpressions(marketingContextFactory:(unit->MarketingContext)) =
-        let mc = marketingContextFactory()
-        let clause = 
-            let scf = mc.Expressions.GetSearchClauseFactory()
-            scf.CreateClause()
-        let so = SearchOptions(PropertiesToReturn = "Id", StartRecord = 1, NumberOfRecordsToReturn = 500)
-        let expressionIds = 
-            let dataSet = mc.Expressions.Search(clause, so)
+
+    let DeleteItems description (searcher:(SearchOptions->DataSet)) deleter = 
+        trace (sprintf "Before delete %s." description)
+        let itemIds = 
+            let so = SearchOptions(PropertiesToReturn = "Id", StartRecord = 1, NumberOfRecordsToReturn = 500)
+            let dataSet = searcher so
             let table = dataSet.Tables.["SearchResults"];
             table.AsEnumerable()
             |> Seq.map (fun row -> row.["Id"] :?> int)
-        for id in expressionIds do
+        for id in itemIds do
             try
-                trace (sprintf "Deleting expression %s" (id.ToString()))
-                mc.Expressions.Delete(id)
+                trace (sprintf "Deleting %s %s" description (id.ToString()))
+                deleter(id)
             with
-            | ex -> trace (sprintf "Failed to delete %s.  Exception: %s" (id.ToString()) ex.Message)
+            | ex -> trace (sprintf "Failed to delete %s with Id %s.  Exception: %s" description (id.ToString()) ex.Message)
+        trace (sprintf "After delete %s." description)
+
+    let DeleteExpressions(marketingContextFactory:(unit->MarketingContext)) =
+        let mc = marketingContextFactory()
+        let description = "Expression"
+        let searcher (so:SearchOptions) = 
+            let clause = 
+                let scf = mc.Expressions.GetSearchClauseFactory()
+                scf.CreateClause()
+            mc.Expressions.Search(clause, so)
+        let deleter id = mc.Expressions.Delete(id)
+        DeleteItems description searcher deleter 
+        ()
+
+    let DeleteCampaignItems (marketingContextFactory:(unit->MarketingContext)) (ciType:CampaignItemType) = 
+        let mc = marketingContextFactory()
+        let description = ciType.ToString() 
+        let searcher (so:SearchOptions) = 
+            let clause = 
+                let scf = mc.CampaignItems.GetSearchClauseFactory(ciType);
+                scf.CreateClause()
+            mc.CampaignItems.Search(clause, so)
+        let deleter id = mc.CampaignItems.Delete(id)
+        DeleteItems description searcher deleter 
         ()
 
     let DeleteDiscounts(marketingContextFactory:(unit->MarketingContext)) =
-        trace "Starting DeleteDiscounts"
-        let mc = marketingContextFactory()
-        let clause = 
-            let scf = mc.CampaignItems.GetSearchClauseFactory(CampaignItemType.Discount);
-            scf.CreateClause()
-        let so = SearchOptions(PropertiesToReturn = "Id", StartRecord = 1, NumberOfRecordsToReturn = 500)
-        let discountIds = 
-            let dataSet = mc.CampaignItems.Search(clause, so, false)
-            let table = dataSet.Tables.["SearchResults"];
-            table.AsEnumerable()
-            |> Seq.map (fun row -> row.["Id"] :?> int)
-        for id in discountIds do
-            try
-                trace (sprintf "Deleting %s" (id.ToString()))
-                mc.CampaignItems.Delete(id)
-            with
-            | ex -> trace (sprintf "Failed to delete %s.  Discount: %s" (id.ToString()) ex.Message)
-        trace "Ending DeleteDiscounts"
+        DeleteCampaignItems marketingContextFactory CampaignItemType.Discount
         ()
-        
 
+    let DeleteAdvertisments(marketingContextFactory:(unit->MarketingContext)) =
+        DeleteCampaignItems marketingContextFactory CampaignItemType.Advertisement
+        ()
 
+    let DeleteDirectMail(marketingContextFactory:(unit->MarketingContext)) =
+        DeleteCampaignItems marketingContextFactory CampaignItemType.DirectMail
+        ()
