30c069d2c8602974ee3d3d7f6a16c2b0a3380d9e
Ben Taylor <ben.taylor.uk@gmail.com>
2012-08-31 16:56:16 +0100

    Updated example script.

--- src/Enticify.CsTasks/ResetDiscounts.fsx
+++ src/Enticify.CsTasks/ResetDiscounts.fsx
@@ -1,31 +1,38 @@
-﻿#r ".\FakeLib.dll"
-#r ".\Enticify.CsTasks.dll"
+﻿//CHANGE THESE REFERENCE PATHS TO POINT AT THE FAKE AND CSTASKS PACKAGE LOCATIONS ON YOUR MACHINE.
+#r @".\src\packages\FAKE.1.64.8\tools\FakeLib.dll"
+#r @".\src\packages\CsTasks.0.1.1-ctp\tools\Enticify.CsTasks.dll"
 
+//Open the namespaces we need
 open CsTasks
 open Fake
 open System
 
+//Setup the Commerce Server site access details.
 let siteName = "StarterSite"
 let mc = MarketingContextSingleton siteName 
+
+//Set up the other details we need.
 let maxTimeSpan = System.TimeSpan.MaxValue
 
 let purgeToolArgs = {
     ToolPath = @"""C:\Program Files (x86)\Microsoft Commerce Server 9.0\Tools\PurgeCommerceData.exe"""
     SiteName = siteName
-    Timeout = maxTimeSpan
-    }
+    Timeout = maxTimeSpan }
 
-let discountImportArgs = 
-    {   MarketingWebServiceUrl = @"""http://localhost/MarketingWebService/MarketingWebService.asmx"""
+let discountImportArgs = {
+        MarketingWebServiceUrl = @"""http://localhost/MarketingWebService/MarketingWebService.asmx"""
         DiscountsPath = @""".\Discount_.xml""" 
         GlobalExpressionsPath = @""".\GlobalExpressions_.xml""" 
         PromoCodesPath = @""".\PromoCodes_.xml""" 
-        Timeout = maxTimeSpan
-    }
+        Timeout = maxTimeSpan }
 
-Target "AnnihilateDiscounts" (fun _ ->
+//Set up our FAKE Targets utilising the CsTasks functions.
+Target "DeleteAndPurgeDiscounts" (fun _ ->
     DeleteDiscounts mc 
     PurgeDiscounts purgeToolArgs 
+)
+
+Target "DeleteExpressions" (fun _ ->
     DeleteExpressions mc 
 )
 
@@ -33,7 +40,8 @@ Target "ImportTestDiscounts" (fun _ ->
     ImportDiscounts discountImportArgs 
 )
 
-"AnnihilateDiscounts" 
-    ==> "ImportTestDiscounts"
+//Set up the Target dependency change (this is a FAKE thing).
+"DeleteAndPurgeDiscounts" ==> "DeleteExpressions" ==> "ImportTestDiscounts"
 
+//Run the Targets.  ImportTestDiscounts depends on the others so they will run too.
 Run "ImportTestDiscounts"
