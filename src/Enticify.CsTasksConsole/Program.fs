da6c396849a0252f454347925d0fac72354d38b9
Ben Taylor <ben.taylor.uk@gmail.com>
2013-03-22 14:59:39 +0000

    Add console app for playing around in.

---
+++ src/Enticify.CsTasksConsole/Program.fs
@@ -0,0 +1,11 @@
+﻿open CsTasks.Marketing
+
+[<EntryPoint>]
+let main argv = 
+    
+    let mds = MarketingDataStore("StarterSite")
+    mds.DeleteAllDiscounts()
+    mds.DeleteAllExpressions()
+    mds.DeleteAllPromoCodeDefinitions()
+    //mds.MakeTestDiscounts(1, 6000)
+    0
